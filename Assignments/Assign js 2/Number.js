58123814a1cde067aab18753b9dd407b198d4c72
madhuk27 <madhusri271997@gmail.com>
2022-06-06 17:35:10 -0500

    commit

--- Assignments/Assign js 2/Number.js
+++ Assignments/Assign js 2/Number.js
@@ -1,8 +1,7 @@
-// random value generated
+// random value 
 var randomnumber = Math.floor(Math.random() * 10 + 1);
 
-// counting the number of guesses
-// made for correct Guess
+// counting the number of guesses are made for correct Guess
 var guess = 1;
 
 document.getElementById("submit").onclick = function(){
@@ -14,8 +13,7 @@ if(guessnumber == randomnumber)
 {	
 alert("Great!! You took " + guess + " attempts to guess it right.");
 }
-else if(guessnumber > randomnumber) /* if guessed number is greater
-            than actual number*/
+else if(guessnumber > randomnumber)
 {	
 guess++;
 alert("Not Really! Give it another try……Try a smaller number");
